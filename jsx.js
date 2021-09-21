4517ccddf406177d0a07cc6b32cb66983193e8e1
traian-007 <72014521+traian-007@users.noreply.github.com>
2021-09-21 08:28:44 +0300

    first sketch

---
+++ jsx.js
@@ -0,0 +1,8 @@
+
+const menuToggle = document.querySelector('.toggle')
+const menu = document.querySelector('.menu')
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active')
+    menu.classList.toggle('active')
+})
